bc52c0ffc414325c26bcab4bcc8b630bb919d4e0
Philip Puk <yanhoipuk@yahoo.com>
2025-12-07 16:39:01 +0000

    feat: Update project dependencies and build configuration
    This commit updates various project dependencies, including React, Three.js, and related libraries, to more recent versions. It also adjusts the Vite build configuration to use relative paths for assets and disables sourcemaps for production builds. Additionally, the application name and version in `package.json` have been updated.

--- index.tsx
+++ index.tsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-const setupOverlay = document.getElementById('setup-overlay');
-
-// If JS executes, we are in a valid environment (server), so hide the warning.
-if (setupOverlay) {
-  setupOverlay.style.display = 'none';
-}
 
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
